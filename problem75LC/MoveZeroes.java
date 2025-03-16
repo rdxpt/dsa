76541bceb54fbd2ff96e4bcb6dc42985f800ace8
rdxpt <rdxpt@outlook.com>
2025-03-16 10:45:28 +0530

    moveZeroesProblem by two approach

---
+++ problem75LC/MoveZeroes.java
@@ -0,0 +1,30 @@
+package problem75LC;
+
+public class MoveZeroes {
+    public static void moveZerosInOrder(int[] nums){
+        int n = nums.length;
+        int nonZeroIndex = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0) nums[nonZeroIndex++]=nums[i++];
+        }
+        while(nonZeroIndex<n)nums[nonZeroIndex++]=0;
+    }
+
+    public static void moveZerosNotInOrder(int[] nums){ 
+        int n = nums.length;
+        int i=0, j=n-1;
+        while(i<j){
+            if(nums[i]!=0)i++;
+            else if(nums[j]==0)j--;
+            else {
+                int temp = nums[i];
+                nums[i++] = nums[j];
+                nums[j--] = temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 4, 1, 1, 6, 0};
+        
+    }
+}
