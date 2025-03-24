713099430ef8daa3c8e2a2e93dbb3b9bba5821ea
rdxpt <rdxpt@outlook.com>
2025-03-24 22:21:24 +0530

    three ques done

---
+++ problem75LC/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,21 @@
+package problem75LC;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GreatestCommonDivisorOfStrings {
+    public String gcdOfString(String str1, String str2){
+        if(!(str1+str2).equals(str2+str1))return "";
+        int len1 = str1.length();
+        int len2 = str2.length();
+        while(len2!=0){
+            int temp = len1;
+            len1 = len2;
+            len2 = temp % len2;
+        };
+        return str1.substring(len1);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ problem75LC/GuessNumberHIgherOrLower.java
@@ -0,0 +1,16 @@
+package problem75LC;
+
+public class GuessNumberHIgherOrLower {
+    public int guessNumber(int n){
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int pick = low+(high-low)/2;
+            int res = guess(pick);
+            if(res==0) return pick;
+            else if(res==-1)high=pick-1;
+            else low=pick+1;
+        }
+        return -1;
+    }
+}
